cecba92921cc71db504d3f1959b03077bd00e092
whoshi <whoshi@v004.vaio.ne.jp>
2023-02-26 21:01:48 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "1657900127-xNEPmVW3";
+    var liffId = "1660668662-rB5PPPaj";
     initializeLiff(liffId);
     
     
