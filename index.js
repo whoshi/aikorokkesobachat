b03c44187a8e34e30384ad3ae0c11c1cfb0244bc
whoshi <whoshi@v004.vaio.ne.jp>
2023-02-26 20:11:18 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+$(function () 
+{
+    
+    // 送信
+    $('form').submit(function () 
+    {
+      
+      var speach1 = document.getElementById("input_speach1").value;
+      
+      if(speach1 == '')
+      {
+	 speach1 = "nodata";     
+      }
+      
+      
+    
+      
+      //入力チェック  
+      if(speach1 === 'nodata')
+      {
+	             alert('入力が何もありません');
+                     return false;	   
+      }
+     
+      
+         
+      
+     //GAS側Postイベントエントリ
+      var url = 'https://script.google.com/macros/s/AKfycbwRaQSOZEtKVh3yLdUlvKBYdNdAm4fmVI4LxPXDr1Qsz1OUSG7C7e9r6wyBpl-JnS6z-Q/exec';
+	         
+                 
+       
+      liff.getProfile().then
+      (profile => 
+        {
+                 // プロフィール名
+                 const name = profile.displayName
+                 var JSONdata = {
+	                                 chatdata:speach1
+                                 };
+                 
+                 //スピナー表示
+                 //インジケータ表示
+                 // Loading 画像を表示
+                 dispLoading("回答生成中...");
+             
+                 $.post(url,
+                        JSONdata,
+                        function(dt)
+                        {
+	                   //インジケータ除去
+	                   // Loading 画像を消す
+                           removeLoading();
+	                   if(dt.message != 'エラーが発生しました')
+	                   {
+                               sendText(dt.message);//To LINE 送信
+	                       liff.closeWindow(); 
+	                   }else
+	                   {
+		               sendText(dt.message);//To LINE 送信
+	                       liff.closeWindow();   
+			       //window.alert("LIFFアプリでエラーが発生");  
+                           }
+	                }
+                 );
+     
+        }
+      );
+      
+      return false;
+    });
+});
