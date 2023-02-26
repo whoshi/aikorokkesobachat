9f67301429ea846cfffb4efef099a8c15f7403ba
whoshi <whoshi@v004.vaio.ne.jp>
2023-02-26 22:45:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ $(function ()
          
       
      //GAS側Postイベントエントリ
-      var url = 'https://script.google.com/macros/s/AKfycbyS6dyt8Jo51CiZp5_xUJa1FhctnnofwGJPzYNk1YqOdkFy5QPeQD2dt9C4OiX3LyYs/exec';
+      var url = 'https://script.google.com/macros/s/AKfycbyiXGm6hkrqT5WW3ULk7VZ6M9_lSwwG_NVpI460IOD4kzAAKFg-eeGRUoVOcku0S0xI/exec';
 	         
                  
        
